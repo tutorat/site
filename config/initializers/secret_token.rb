e5a55d862366af3c38f33df3b452edff17611ad1
Etienne Richan <tutoratenligne@gmail.com>
2014-03-26 15:33:03 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TutorSite::Application.config.secret_key_base = '476865ace34c928807cd0b22582eea5388b07bfc984aa0905852bfdbc91f3b6635348f2b3ca93cbd7ddccc9c6da212fd66ca71d37bbd07fa602609904044cc0b'
